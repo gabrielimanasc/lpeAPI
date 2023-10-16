5e43628a1c9d7776368e09db8c51d1e5da321dd4
Gabriel Lima do Nascimento <gabrielimanasc@gmail.com>
2023-10-15 23:39:28 -0300

    FIxing route

--- usecases/categoriasUseCase.js
+++ usecases/categoriasUseCase.js
@@ -1,3 +1,15 @@
+const { pool } = require('../config')
+const Categoria = require('../entities/categoria');
+
+const getCategoriasDB = async () => {
+    try {
+        const { rows } = await pool.query(`SELECT * FROM categorias ORDER BY nome`)
+        return rows.map((categoria) => new Categoria(categoria.codigo, categoria.nome));
+    } catch (err) {
+        throw "Erro: " + err;
+    }
+}
+
 const addCategoriaDB = async (body) => {
     try {
         const { nome } = body;
